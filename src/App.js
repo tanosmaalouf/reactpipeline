73ba4ce08c1815a7a134912e85a6a1fef79310a2
tanosmaalouf <maalouf466@gmail.com>
2023-11-28 14:50:22 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and saves to reload.
-          w bzazaaaaaaaaa
+          w ottttttttttttttttttttttttttttttttttttttttttttttt
         </p>
         <a
           className="App-link"
